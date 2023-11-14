bd26cc4e0d35f13138448b8f07c2f76e979d2e99
Jack Weru <werukioni@gmail.com>
2023-11-14 21:22:35 +0300

    UI implementation complete; Configuring endpoints and APIs in-progress...

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -3,16 +3,15 @@ import { mpesa_api_callback_endpoint } from "@/utils/lipa_na_mpesa";
 import { NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest, res: NextApiResponse) => {
+export const POST = async (req: NextRequest, res: NextResponse) => {
     try {
         const incomingRequest = await req.json();
         const mpesa_api_callback: MPESA_CALLBACK_DOCS_STORE_TYPE = incomingRequest?.Body?.stkCallback
         await mpesa_api_callback_endpoint(mpesa_api_callback);
-        
-        res.status(200).send("");
+        return new NextResponse(JSON.stringify("OK"), { status: 200 });
     } catch (error) {
         console.log("Error at callback url: ", error);
-        res.status(200).send("");
+        return new NextResponse(JSON.stringify("OK"), { status: 200 });
     }
 }
 
