0c43b3fbfb60bb440505454842d477197d78a985
Jack Weru <werukioni@gmail.com>
2023-11-14 19:11:34 +0300

    UI implementation complete; Configuring endpoints and APIs in-progress...

--- app/api/food_menu/route.ts
+++ app/api/food_menu/route.ts
@@ -8,6 +8,8 @@ export const GET = ():NextResponse => {
         { id: 3, name: "Food3", price: 3, vat: 3 },
         { id: 4, name: "Food4", price: 4, vat: 4 },
         { id: 5, name: "Food5", price: 5, vat: 5 },
+        { id: 6, name: "Food6", price: 6, vat: 6 },
+        { id: 7, name: "Food7", price: 7, vat: 7 },
     ];
 
     return NextResponse.json(foodMenuList);
