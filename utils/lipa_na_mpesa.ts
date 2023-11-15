66b3f9f0bf2c56d28a1851ba396fb286e01e16dc
Jack Weru <werukioni@gmail.com>
2023-11-15 10:52:57 +0300

    UI design and implementation complete; Api and Endpoint configuration in-progress...

--- utils/lipa_na_mpesa.ts
+++ utils/lipa_na_mpesa.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { MPESA_CALLBACK_DOCS_STORE_TYPE, MpesaAcceptedPendingCallbackType } from "./Interface";
 import { redis } from "./redis_config";
 
-export function editMpesaNumber(contact: string | null = null) {
+export function editMpesaNumber(phoneNumber: string) {
+    const contact = phoneNumber;
     if (!contact) { 
         return null;
     }
