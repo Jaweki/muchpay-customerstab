5f029dc1a62c114e7b8202c93d8ed6816e089184
Jaweki <werukioni@gmail.com>
2024-11-01 02:52:02 +0300

    updated routes of /api; adjusted the res and req parameters for the route functions;

--- app/api/food_menu/route.ts
+++ app/api/food_menu/route.ts
@@ -1,22 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connectToDB } from "@/utils/database";
 import MenuSchema from "@/models/MenuSchema";
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
+export const GET = async () => {
+  try {
+    const isConnected = await connectToDB();
 
-    try {
-        const isConnected = await connectToDB();
-
-        if (!isConnected) {
-            throw new Error("Failed to connect to db...");
-        }
-
-        const menu = await MenuSchema.find({});
-
-        return NextResponse.json(menu);
-    } catch (error) {
-        console.log("Error in the fetch food_menu Endpoint: ", error);
-        return new NextResponse(JSON.stringify([]), { status: 500});
+    if (!isConnected) {
+      throw new Error("Failed to connect to db...");
     }
-    
-}
+
+    const menu = await MenuSchema.find({});
+
+    return NextResponse.json(menu);
+  } catch (error) {
+    console.log("Error in the fetch food_menu Endpoint: ", error);
+    return new NextResponse(JSON.stringify([]), { status: 500 });
+  }
+};
--- app/api/lipa/route.ts
+++ app/api/lipa/route.ts
@@ -1,76 +1,96 @@
-import { ConfirmDataProps, MPESA_CALLBACK_DOCS_STORE_TYPE, OrderData } from "@/utils/Interface";
+import {
+  ConfirmDataProps,
+  MPESA_CALLBACK_DOCS_STORE_TYPE,
+  OrderData,
+} from "@/utils/Interface";
 import { writeDetailsOfCompleteOrderToDB } from "@/utils/database";
-import { editMpesaNumber, requestMpesaPayment} from "@/utils/lipa_na_mpesa";
+import { editMpesaNumber, requestMpesaPayment } from "@/utils/lipa_na_mpesa";
 import { NextRequest, NextResponse } from "next/server";
 
 export const maxDuration = 120;
 type MpesaApiResponseType = {
-    status: string,
-    resultData?: MPESA_CALLBACK_DOCS_STORE_TYPE;
-    error_message?: string;
-    error_code?: number;
-}
+  status: string;
+  resultData?: MPESA_CALLBACK_DOCS_STORE_TYPE;
+  error_message?: string;
+  error_code?: number;
+};
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
+  try {
+    const { order, customer }: OrderData = await req.json();
+    console.log("Received order and customer data:", order, customer);
 
-    try {
-        const {order, customer}: OrderData = await req.json();
-        console.log('Received order and customer data:', order, customer);
+    let total_bill = 0;
+    order.forEach((food) => {
+      total_bill += food.price;
+    });
 
-        let total_bill = 0;
-        order.forEach((food) => {
-            total_bill += food.price;
-        })
+    const mpesaNumber = editMpesaNumber(customer.contact);
 
-        const mpesaNumber = editMpesaNumber(customer.contact);
-
-        if (!mpesaNumber) {
-            const response: any = {
-                status: "error",
-                error_message: "Invalid Mpesa Number",
-                error_code: 400
-            }
-            throw new Error(response);
-        }
-
-        const BSshortcode = 174379;
-
-        const mpesa_api_response: MpesaApiResponseType = await requestMpesaPayment(BSshortcode, mpesaNumber, total_bill);
-
-        if (mpesa_api_response.error_message && mpesa_api_response.status === 'error') {
-            const payload: ConfirmDataProps = {
-                message: mpesa_api_response.error_message,
-                error_code: mpesa_api_response.error_code
-            }
-            return new NextResponse(JSON.stringify({ fail_message: payload }), { status: 200});
-        }
-
-        if (!mpesa_api_response?.resultData?.CallbackMetadata) {
-            const payload: ConfirmDataProps = {
-                message: "System error",
-                error_code: 500,
-            }
-            return new NextResponse(JSON.stringify({ fail_message: payload }), { status: 200});
-        }
-
-        const result = await writeDetailsOfCompleteOrderToDB(order, mpesa_api_response?.resultData?.CallbackMetadata, mpesa_api_response.resultData?.MerchantRequestID)
-
-        if (result !== true) {
-            throw new Error("Failed to write Order detailes to db.")
-        }
-
-        const payload: ConfirmDataProps = {
-            message: mpesa_api_response.resultData?.ResultDesc ?? "Transaction Successful",
-            receipt_no: mpesa_api_response.resultData?.MerchantRequestID,
-            mpesa_refNo: mpesa_api_response?.resultData?.CallbackMetadata?.mpesaReceiptNumber
-        }
-        return new NextResponse(JSON.stringify({ success: payload }), { status: 201});
-
-    } catch (error: any) {
-        return new NextResponse(JSON.stringify({ error: "Internal System Error!" }), { status : 500});
+    if (!mpesaNumber) {
+      const response: any = {
+        status: "error",
+        error_message: "Invalid Mpesa Number",
+        error_code: 400,
+      };
+      throw new Error(response);
     }
-        
-    
-}
 
+    const BSshortcode = 174379;
 
+    const mpesa_api_response: MpesaApiResponseType = await requestMpesaPayment(
+      BSshortcode,
+      mpesaNumber,
+      total_bill
+    );
+
+    if (
+      mpesa_api_response.error_message &&
+      mpesa_api_response.status === "error"
+    ) {
+      const payload: ConfirmDataProps = {
+        message: mpesa_api_response.error_message,
+        error_code: mpesa_api_response.error_code,
+      };
+      return new NextResponse(JSON.stringify({ fail_message: payload }), {
+        status: 200,
+      });
+    }
+
+    if (!mpesa_api_response?.resultData?.CallbackMetadata) {
+      const payload: ConfirmDataProps = {
+        message: "System error",
+        error_code: 500,
+      };
+      return new NextResponse(JSON.stringify({ fail_message: payload }), {
+        status: 200,
+      });
+    }
+
+    const result = await writeDetailsOfCompleteOrderToDB(
+      order,
+      mpesa_api_response?.resultData?.CallbackMetadata,
+      mpesa_api_response.resultData?.MerchantRequestID
+    );
+
+    if (result !== true) {
+      throw new Error("Failed to write Order detailes to db.");
+    }
+
+    const payload: ConfirmDataProps = {
+      message:
+        mpesa_api_response.resultData?.ResultDesc ?? "Transaction Successful",
+      receipt_no: mpesa_api_response.resultData?.MerchantRequestID,
+      mpesa_refNo:
+        mpesa_api_response?.resultData?.CallbackMetadata?.mpesaReceiptNumber,
+    };
+    return new NextResponse(JSON.stringify({ success: payload }), {
+      status: 201,
+    });
+  } catch (error: any) {
+    return new NextResponse(
+      JSON.stringify({ error: "Internal System Error!" }),
+      { status: 500 }
+    );
+  }
+};
